db6a38912aad1347c2abc9fa3c7767a0f2b9231b
bogdan.crisan <bogdan.18rap@gmail.com>
2019-02-08 12:52:57 +0200

    test-commit

--- testing_git_object.py
+++ testing_git_object.py
@@ -21,31 +21,38 @@ def todo_job():
 
 
 def handle_git():
-    repo = git.Repo.clone_from("https://github.com/bccrisan/ciduty_tools", os.path.join(os.getcwd(), 'testing_ciduty_tools'), branch='master')
+    # repo = git.Repo.clone_from("https://github.com/bccrisan/ciduty_tools", os.path.join(os.getcwd(), 'testing_ciduty_tools'), branch='master')
 
-    empty_repo = git.Repo.init(os.path.join(os.getcwd(), 'testing_ciduty_tools'))
-    origin = empty_repo.remote("origin")
-    origin.fetch()  # assure we actually have data. fetch() returns useful information
-    origin.pull()   # Pull from remote origin the last known and good state of the code.
+    # empty_repo = git.Repo.init(os.path.join(os.getcwd(), 'testing_ciduty_tools'))
+    # origin = empty_repo.remote("origin")
+    # origin.fetch()  # assure we actually have data. fetch() returns useful information
+    # origin.pull()   # Pull from remote origin the last known and good state of the code.
 
     todo_job()      # The job to do before pushing.
     
-    # Generate the path for the modified files. (in this case it is hardcoded)
-    path = "E:\\bcrisan\\repos\\bcrisan\\testing_git_clonning\\testing_ciduty_tools\\demofile.txt"
+    # # Generate the path for the modified files. (in this case it is hardcoded)
+    # path = "E:\\bcrisan\\repos\\bcrisan\\testing_git_clonning\\testing_ciduty_tools\\demofile.txt"
     
-    # Add the path of the modified file to the repo. (similar with "git add "specific file" ")
-    repo.index.add([path])
+    # # Add the path of the modified file to the repo. (similar with "git add "specific file" ")
+    # repo.index.add([path])
 
-    # Commiting changes with a generated commit message (git commit -m "Test commit + UTCNOW Date (GMT + 0)")
-    repo.index.commit("Test commit: " + str(datetime.datetime.utcnow()))
+    # # Commiting changes with a generated commit message (git commit -m "Test commit + UTCNOW Date (GMT + 0)")
+    # repo.index.commit("Test commit: " + str(datetime.datetime.utcnow()))
     
-    #Pushing the commit to the origin. (master branch,)
-    origin.push()
+    # #Pushing the commit to the origin. (master branch,)
+    # origin.push()
+    os.system("git add .")
+    time.sleep(1)
+    os.system("git commit - m \"Test commit: " + str(datetime.datetime.utcnow()) + "\"")
+    time.sleep(1)
+    os.system("git push origin master")
+
+
 
 if __name__ == "__main__":
-    path = os.getcwd()
-    path = path + "\\testing_ciduty_tools"
-    print(path)   
+    # path = os.getcwd()
+    # path = path + "\\testing_ciduty_tools"
+    # print(path)   
     handle_git()
     
-    os.system("rmdir /S /Q \"{}\"".format(path))
+    # os.system("rmdir /S /Q \"{}\"".format(path))
